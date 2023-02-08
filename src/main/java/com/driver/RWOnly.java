2f743a82b3bad6603175e9a455f9b9b8f1db5c2e
Akash Sharma <akash.bohareji@gmail.com>
2023-02-08 14:55:15 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String  name="Akash";
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
