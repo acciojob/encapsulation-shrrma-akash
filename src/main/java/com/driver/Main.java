92bff7c612171449a95946eada8970521ba8090c
Akash Sharma <akash.bohareji@gmail.com>
2023-02-08 14:54:23 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+
+        //rw.name = "akash";
+        //System.out.println(name);
+        //java : name has private access in com.driver.RWOnly
+
+        rw.setName("Akash");
+
+        // get number...
+        String ans= rw.getName();
+        System.out.println(ans);
+    }
 }
